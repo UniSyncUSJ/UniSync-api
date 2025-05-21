41a5fa596051674bcd32187850bb6705bbae2451
Ranga Bandara <rangabandara1729@gmail.com>
2025-05-21 15:14:48 +0530

    Dependency issue fixed

--- src/main/java/com/uniSync/uniSync_api/UniSyncApiApplication.java
+++ src/main/java/com/uniSync/uniSync_api/UniSyncApiApplication.java
@@ -8,6 +8,7 @@ public class UniSyncApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UniSyncApiApplication.class, args);
-	}
+		System.out.println("Hello!");
 
+	}
 }

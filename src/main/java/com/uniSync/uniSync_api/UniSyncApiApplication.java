aba42bf068e9f7dd651de5d888b949d0e3582573
Ranga Bandara <rangabandara1729@gmail.com>
2025-05-21 15:00:22 +0530

    Backend initalized

---
+++ src/main/java/com/uniSync/uniSync_api/UniSyncApiApplication.java
@@ -0,0 +1,13 @@
+package com.uniSync.uniSync_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniSyncApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniSyncApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniSync/uniSync_api/UniSyncApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.uniSync.uniSync_api;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniSyncApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

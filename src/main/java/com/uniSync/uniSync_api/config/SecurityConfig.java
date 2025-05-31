8a3a61b680d0edc255b505b02464621c591f10d1
rusirasandul <rusira.202311180@iit.ac.lk>
2025-05-31 12:00:11 +0530

    Test complete and Running

--- src/main/java/com/uniSync/uniSync_api/config/SecurityConfig.java
+++ src/main/java/com/uniSync/uniSync_api/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
-            .antMatchers("/auth/login", "/").permitAll()
+            .requestMatchers("/auth/login", "/").permitAll()
             .anyRequest().authenticated()
             .and()
             .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
